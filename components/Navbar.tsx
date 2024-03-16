b05241ae63bcfc97838c35d3854b5807df58acfd
WindXRan <3220732889@qq.com>
2024-03-16 17:27:31 +0800

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,28 +10,34 @@ import {
 
 const Navbar = () => {
   return (
-    <nav className="fixed w-full z-40">
-      <div className="px-4 md:px-16 py-6 flex flex-row bg-[#853131]">
-        <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
-        <div
-          className="
-                    flex-row
-                    ml-8
-                    gap-7
-                    hidden
-                    lg:flex"
-        >
-          <NavbarItem label="历史" />
-          <NavbarItem label="功能" />
-          <NavbarItem label="地域" />
-          <NavbarItem label="制作" />
-          <NavbarItem label="表演" />
-          <NavbarItem label="展览" />
+    <nav className=" fixed top-0 left-0 w-full flex items-center justify-between px-100 md:px-16 py-6 bg-[#853131] z-40">
+           {" "}
+      <div className="flex items-center flex-grow mx-8 lg:mx-12">
+               {" "}
+        <div className="flex items-center gap-4 lg:gap-7 flex-grow justify-end ">
+                    <NavbarItem label="历史" />
+                    <NavbarItem label="功能" />
+                    <NavbarItem label="地域" />       {" "}
         </div>
-        <div className="ml-auto">
-          <Button variant="ghost">切换</Button>
+               {" "}
+        <div className="flex flex-grow justify-center">
+                   {" "}
+          <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />     
+           {" "}
         </div>
+               {" "}
+        <div className="flex items-center gap-4 lg:gap-7 flex-grow justify-start">
+                    <NavbarItem label="制作" />
+                    <NavbarItem label="表演" />
+                    <NavbarItem label="展览" />       {" "}
+        </div>
+             {" "}
       </div>
+           {" "}
+      <div className="ml-auto">
+                <Button variant="ghost">切换</Button>     {" "}
+      </div>
+         {" "}
     </nav>
   );
 };
